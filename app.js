a0264827ee6b022c20f75b5b5180dc32f9f60d86
wangshun <513726532@qq.com>
2019-12-05 18:59:41 +0800

    page生命周期

---
+++ app.js
@@ -0,0 +1,54 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    //异步调用
+    // wx.getUserInfo({
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    switch(options.scene){
+      case 1001:
+      break;
+      case 1005:
+      break;
+    }
+
+    //2.获取用户信息，发给服务器
+    // wx.getUserInfo({
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log("小程序中发生了一些错误")
+  },
+
+  globalData:{
+    name:'codewhy',
+    age:18
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,46 +1,80 @@
 // pages/home/home.js
+// const app = getApp()
+
+// const name = app.globalData.name;
+// const age = app.globalData.age;
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    name:'Hukeke',
+    // age:18,
+    // students:[
+    //   { id: 110, name: 'age', age: 18 },
+    //   { id: 110, name: 'height', age: 165 },
+    //   { id: 110, name: 'weight', age: 110 },
+    // ],
+    // counter:0
 
   },
+  handleBtnClick(){
+    //1.错误的做法：界面不会刷新
+    // this.data.counter+=1
+    // console.log(this.data.counter)
 
+    //2.this.setData()
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter-1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success(res){
+        console.log(res)
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload')
   },
 
   /**
